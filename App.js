98d9a40734dc672313be4aa083406cb4b50264ea
Nicholas Tauroa <nicholas.tauroa@developersinstitute.co.nz>
2023-09-08 17:06:44 +1200

    fonts added, Bill component added

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from "expo-status-bar";
-import SvgUri from "react-native-svg";
 import { StyleSheet, Text, View, TextInput, Image } from "react-native";
+import { useState } from "react";
 
 function Bill() {
   const [bill, setBill] = useState(null);
@@ -11,13 +11,14 @@ function Bill() {
 
   return (
     <View style={styles.billComp}>
-      <Text>Bill</Text>
+      <Text style={styles.billText}>Bill</Text>
       <TextInput
         style={styles.textInput}
         placeholder="Bill Amount"
         onChangeText={billAmount}
         value={bill}
       />
+      {bill ? <Text>{bill}</Text> : null}
     </View>
   );
 }
@@ -25,8 +26,13 @@ function Bill() {
 export default function App() {
   return (
     <View style={styles.container}>
-      <SvgUri width="auto" height="auto" source={require("./imgs/logo.svg")} />
-      <Bill />
+      <View style={styles.img}>
+        <Image styels={styles.img} source={require("./imgs/logo.png")} />
+      </View>
+      <View style={styles.appBox}>
+        <Text>App Go here</Text>
+        <Bill />
+      </View>
     </View>
   );
 }
@@ -34,8 +40,41 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: "rgba(197, 228, 231, 255)",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  img: {
+    flex: 0,
+    height: 200,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  appBox: {
+    borderTopLeftRadius: 30,
+    borderTopRightRadius: 30,
+    flex: 1,
+    height: "60%",
+    width: "100%",
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+
+  billComp: {
+    flex: 1,
+    fontSize: 40,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  billText: {
+    flex: 1,
+    fontSize: 40,
+    textAlign: "left",
+    justifyContent: "start",
   },
 });
