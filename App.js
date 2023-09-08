62e8276736ca7cd921ea3e31d84bc4995b4d4b24
Nicholas Tauroa <nicholas.tauroa@developersinstitute.co.nz>
2023-09-08 15:00:14 +1200

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import SvgUri from "react-native-svg";
+import { StyleSheet, Text, View, TextInput, Image } from "react-native";
+
+function Bill() {
+  const [bill, setBill] = useState(null);
+
+  const billAmount = (inputAmount) => {
+    setBill(inputAmount);
+  };
+
+  return (
+    <View style={styles.billComp}>
+      <Text>Bill</Text>
+      <TextInput
+        style={styles.textInput}
+        placeholder="Bill Amount"
+        onChangeText={billAmount}
+        value={bill}
+      />
+    </View>
+  );
+}
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <SvgUri width="auto" height="auto" source={require("./imgs/logo.svg")} />
+      <Bill />
     </View>
   );
 }
@@ -13,8 +34,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
